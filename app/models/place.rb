1185f7491b53c3fb005f1d5bd95ca3a5bc7cce4d
Akshai Rajendran <akshai.rajendran@gmail.com>
2016-07-06 02:24:46 +0000

    More validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, too_short: "Please enter at least 3 characters"}
+  validates :address, presence: true
+  validates :description, presence: true
 end
