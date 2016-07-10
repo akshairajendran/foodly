5656c1e94fd34d55730f3d6dc33315cd7a153a2a
Akshai Rajendran <akshai.rajendran@gmail.com>
2016-07-10 22:54:53 +0000

    Add photo captions

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  has_many :photos
 
   validates :name, presence: true, length: { minimum: 3, too_short: "Please enter at least 3 characters"}
   validates :address, presence: true
